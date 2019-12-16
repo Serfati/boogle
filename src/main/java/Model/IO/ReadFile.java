039b20730881870fe48c1725300e967daaf2ea2c
serfati <avihaipp@gmail.com>
2019-12-16 09:59:34 +0200

    16.12

--- src/main/java/Model/IO/ReadFile.java
+++ src/main/java/Model/IO/ReadFile.java
@@ -1,21 +1,17 @@
 package Model.IO;
 
 import Model.Parser.cDocument;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Objects;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -56,44 +52,6 @@ public class ReadFile {
             e.printStackTrace();
         }
         return set;
-    }
-}
-
-class Reader implements Callable<LinkedList<cDocument>> {
-    File file;
-
-    Reader(File file) {
-        this.file = file;
-    }
-
-    @Override
-    public LinkedList<cDocument> call() {
-        File[] directoryListing = file.listFiles();
-        LinkedList<cDocument> fileList = new LinkedList<>();
-        if (directoryListing != null && file.isDirectory())
-            Arrays.stream(directoryListing).map(this::read).forEach(fileList::addAll);
-        return fileList;
-    }
-
-    private LinkedList<cDocument> read(File fileToSeparate) {
-        LinkedList<cDocument> docList = new LinkedList<>();
-        try {
-            Document document = Jsoup.parse(new String(Files.readAllBytes(Objects.requireNonNull(file.listFiles())[0].toPath())));
-            assert document != null;
-            Elements docElements = document.getElementsByTag("DOC");
-            for(Element element : docElements) {
-                Elements IDElement = element.getElementsByTag("DOCNO");
-                Elements DateElement = element.getElementsByTag("DATE1");
-                Elements TitleElement = element.getElementsByTag("TI");
-                Elements TextElement = element.getElementsByTag("TEXT");
-                cDocument newDoc = new cDocument(fileToSeparate.getName(), IDElement.text(), DateElement.text(), TitleElement.text(), TextElement.text());
-                docList.add(newDoc);
-            }
-            return docList;
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-        return docList;
     }
 }
 
